6135cfd3d381a3e6c52fe2d77d6fb48cee5f8982
MethoD112 <mileshko.vitaly@gmail.com>
2017-04-17 20:44:01 +0300

    task#10

--- app.js
+++ app.js
@@ -21,62 +21,65 @@ db.connect('db', ['articles', 'tags']);
 
 // get request for getting articles from database for first time load app
 app.get('/articles', (req, res) => {
-    const articles = db.articles.find();
-    res.send(articles);
+    const promiseArticle = new Promise((resolve, reject) => {
+        resolve(db.articles.find());
+    });
+
+    promiseArticle.then(result => res.send(result));
 });
 
 // get request for getting tags from database for first time load app
 app.get('/tags', (req, res) => {
+    const promiseTags = new Promise((resolve, reject) => {
+        resolve(db.tags.find());
+    });    
     const tags = db.tags.find();
-    res.send(tags);
+    promiseTags.then(result => res.send(result));
 });
 
 // post request for adding article to database
 app.post('/articles', (req, res) => {
-    const articlesString = req.body;
-    db.articles.save(articlesString);
-
-    const article = db.articles.find({id: articlesString.id});
-    res.send(article);
+    const promiseAddArticle = new Promise((resolve, reject) => {
+        db.articles.save(req.body);
+        resolve(db.articles.find({id: req.body.id}));
+    });    
+    promiseAddArticle.then(result => res.send(result));
 });
 
 // post request for adding tag to database
 app.post('/posttags', (req, res) => {
-    const tagString = req.body.tag;
-    db.tags.save(tagString);
-    const tags = db.tags.find();
-    res.send(tags);
+    const promiseAddTag = new Promise((resolve, reject) => {
+        db.tags.save(req.body.tag);
+        resolve(db.tags.find());
+    });    
+    promiseAddTag.then(result => res.send(result));
 });
 
 // post request for editting article in database
 app.post('/editarticle', (req, res) => {
-    const articleString = req.body;
+    const promiseEditArticle = new Promise((resolve, reject) => {
+        const articleString = req.body;
+        const query = { id: articleString.id };
 
-    const query = { id: articleString.id };
+        const options = {
+           multi: false,
+           upsert: false
+        };
 
-    const options = {
-       multi: false,
-       upsert: false
-    };
-
-    db.articles.update(query, articleString, options);
-
-    const article = db.articles.find(query);
-
-    res.send(article);
+        db.articles.update(query, articleString, options);
+        resolve(db.articles.find(query));
+    });    
+    promiseEditArticle.then(result => res.send(result));
 });
 
 // post request for deleting article in database
 app.post('/deletearticle', (req, res) => {
-    const id = req.body;
-
-    const bool = db.articles.remove(id);
-    if (bool) {
-        res.send(id);
-    } else {
-        res.send('No such article in database');
-    }
-    
+    const promiseDeleteArticle = new Promise((resolve, reject) => {
+        const id = req.body;
+        db.articles.remove(id);
+        resolve(id);
+    });    
+    promiseDeleteArticle.then(result => res.send(result));
 });
 
 app.listen(3000, () => { console.log('Port:3000'); });
