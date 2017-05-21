adb18777dc3e558f84a6c3402250513b8dd024c3
MethoD112 <mileshko.vitaly@gmail.com>
2017-05-21 18:36:51 +0300

    deplou app on heroku

--- app.js
+++ app.js
@@ -21,4 +21,5 @@ app.use('/', images);
 app.use('/', users);
 
 // Start server
-app.listen(3000, () => { console.log('Port:3000'); });
+const port = process.env.PORT || 3000;
+app.listen(port, () => { console.log('Port:3000'); });
